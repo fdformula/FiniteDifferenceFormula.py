49acb52d5465c9469549d016396cb5a1254f9573
David Wang <64943188+Winux2k@users.noreply.github.com>
2023-01-30 17:41:37 -0500

    Add files via upload

--- src/FiniteDifferenceFormula/__init__.py
+++ src/FiniteDifferenceFormula/__init__.py
@@ -1,5 +1,13 @@
-__all__ = [ \
-  "compute", "find", "findforward", "findbackward", "formula", \
-  "truncationerror", "decimalplaces", "verifyformula", \
-  "activatepythonfunction", "taylor", "printtaylor", \
-  "fde", "fde1", "fdd"]
+# __all__ = [ \
+#   "fd", "compute", "find", "findforward", "findbackward", "formula", \
+#   "truncationerror", "decimalplaces", "verifyformula", \
+#   "activatepythonfunction", "taylor", "printtaylor", \
+#   "fde", "fde1", "fdd"]
+
+# def main():
+#     fd = FiniteDifferenceFormula()
+#     print("Object fd is available. You may run commands like\n",
+#           "fd.compute(2, range(-10,11))\n",
+#           "fd.compute(1, [0, -1, -2, 3, 4, 7])\n",
+#           "fd.activatepythonfunction()\n",
+#           "fd.truncationerror()", sep = '')
