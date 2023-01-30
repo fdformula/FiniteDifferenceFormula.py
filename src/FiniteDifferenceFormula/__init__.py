bf3126bdb53778358d33cd7c33664e39056842e5
David Wang <64943188+Winux2k@users.noreply.github.com>
2023-01-30 16:40:53 -0500

    Add files via upload

---
+++ src/FiniteDifferenceFormula/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [ \
+  "compute", "find", "findforward", "findbackward", "formula", \
+  "truncationerror", "decimalplaces", "verifyformula", \
+  "activatepythonfunction", "taylor", "printtaylor", \
+  "fde", "fde1", "fdd"]
